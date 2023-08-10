46c81a7adde2baa781e8eb78c4c47576a70d7e8e
Matiasmiño <108136043+matiasminoo@users.noreply.github.com>
2023-08-10 12:35:47 -0300

    adding ex 1

---
+++ ej1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n", a[i]);
+    }
+
+    return 0;
+}
