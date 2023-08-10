a670039a79e09c03d440fecf3b46847606f042ef
Matiasmiño <108136043+matiasminoo@users.noreply.github.com>
2023-08-10 12:41:14 -0300

    adding ex 2

---
+++ ej2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int n = atoi(argv[1]);
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = i;
+        printf("%d \n", a[i]);
+    }
+    return 0;
+}
